acfd80595761df0ac55cba30db3ed42c88ec5289
Khaled Altabtbaei <altabtba@ualberta.ca>
2022-03-24 16:14:02 -0600

    Update alpha_generate.R

--- workflow/scripts/alpha_generate.R
+++ workflow/scripts/alpha_generate.R
@@ -8,6 +8,15 @@ option_list = list(
   make_option(c("-o", "--output"), type="character", default=NULL, help="Name of the output file", metavar="Output File name")
 );
 
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+if (is.null(opt$input)){
+  print_help(opt_parser)
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+}
+
+
 biom = opt$input
 alpha = opt$alpha
 output = opt$output
