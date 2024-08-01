19dfe0ce28d206e9c38e14c30bf2b53f3a002a3e
Khaled Altabtbaei <36083988+khalidtab@users.noreply.github.com>
2024-07-31 23:39:29 -0600

    Update DAtest.post-deploy.sh

--- workflow/envs/DAtest.post-deploy.sh
+++ workflow/envs/DAtest.post-deploy.sh
@@ -2,4 +2,4 @@ conda activate $CONDA_PREFIX
 R -e 'if (!requireNamespace("devtools")) install.packages("devtools") ; devtools::install_github("Russel88/DAtest", upgrade = "never")'
 R -e 'devtools::install_version("statmod", version = "1.4.33", repos ="http://cran.us.r-project.org”, upgrade = "never") ; devtools::install_version("cplm", version = "0.7-5", repos = "http://cran.us.r-project.org", upgrade = "never")'
 R -e 'devtools::install_github("himelmallick/Tweedieverse", upgrade = "never")'
-R -e 'install.packages("ROCR")'
+R -e 'install.packages("ROCR"، repos = "http://cran.us.r-project.org", upgrade = "never")'
